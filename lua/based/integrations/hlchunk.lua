bd3b8c772f0fc272f5bbf01d9c9b9a060199184a
Satanx016 <z.dadsi21@gmail.com>
2024-12-15 18:36:24 +0100

    feat(hlchunk): add highlight for line_num feat

--- lua/based/integrations/hlchunk.lua
+++ lua/based/integrations/hlchunk.lua
@@ -1,7 +1,9 @@
+local theme = require("based").get_theme_tb("base_16")
 local colors = require("based").get_theme_tb("base_30")
 
 return {
   HLChunk1 = { fg = colors.light_grey },
   HLChunk2 = { fg = colors.red },
   HLIndent1 = { fg = colors.line },
+  HLLineNum1 = { fg = theme.base05 },
 }
